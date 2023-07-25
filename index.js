9f83d0825a642b9a1936553bf25c2d73b8d12fd2
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-25 10:02:53 +0800

    feat: add hand shape reconcilation logic

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ function getComputerChoice() {
   const randomChoice = Math.floor(Math.random() * 3)
   return choices[randomChoice]
 }
+
+function singleRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase()
+  computerSelection = computerSelection.toLowerCase()
+
+  if (playerSelection === computerSelection) {
+    return [-1, "It's a tie!"]
+  } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+    return [1, 'You Win! Rock beats Scissors!']
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return [0, 'You Lose! Paper beats Rock!']
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return [1, 'You Win! Paper beats Rock!']
+  } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+    return [0, 'You Lose! Scissors beats Paper!']
+  } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+    return [1, 'You Win! Scissors beats Paper!']
+  } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+    return [0, 'You Lose! Rock beats Scissors!']
+  }
+}
