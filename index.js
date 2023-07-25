28abbe268fc99d42a4f40d14f7e02de84a7dbc88
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-25 10:03:26 +0800

    feat: add main game logic

--- index.js
+++ index.js
@@ -25,3 +25,35 @@ function singleRound(playerSelection, computerSelection) {
     return [0, 'You Lose! Rock beats Scissors!']
   }
 }
+
+function game() {
+  let userScore = 0
+  let computerScore = 0
+
+  while (userScore < 5 && computerScore < 5) {
+    const userSelection = window.prompt(
+      'Enter your choice (rock, paper, scissor): '
+    )
+    const computerSelection = getComputerChoice()
+
+    const [userWon, message] = singleRound(userSelection, computerSelection)
+
+    if (userWon === 1) {
+      userScore++
+    } else if (userWon === 0) {
+      computerScore++
+    }
+
+    console.log(message, `[${userScore} - ${computerScore}]`)
+  }
+
+  if (userScore > computerScore) {
+    console.log(`You Win the game for a score of ${userScore} `)
+  } else if (userScore < computerScore) {
+    console.log(`You Lose the game for a score of ${userScore}`)
+  } else {
+    console.log('Player and Computer have a score tie!')
+  }
+}
+
+game()
